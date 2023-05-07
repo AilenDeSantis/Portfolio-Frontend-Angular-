df1399fad83e835c2a45f4b562ca6ef005b39d19
AilenDeSantis <ailenm.de.santis@gmail.com>
2023-05-07 15:24:50 -0300

    Agrego favicon y componente de inicio de sesión sin funcionalidad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
 import { AboutComponent } from './componentes/about/about.component';
@@ -9,7 +9,16 @@ import { EducationComponent } from './componentes/education/education.component'
 import { CoursesComponent } from './componentes/courses/courses.component';
 import { SkillsComponent } from './componentes/skills/skills.component';
 import { ProjectsComponent } from './componentes/projects/projects.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { IniciarSesionComponent } from './componentes/iniciar-sesion/iniciar-sesion.component';
+import { PortfolioComponent } from './componentes/portfolio/portfolio.component'
+import { ReactiveFormsModule } from "@angular/forms";
+
+const routes: Routes = [
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: 'iniciar-sesion', component: IniciarSesionComponent},
+  {path: '', redirectTo: 'iniciar-sesion', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -20,11 +29,15 @@ import { HttpClientModule } from '@angular/common/http'
     EducationComponent,
     CoursesComponent,
     SkillsComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    IniciarSesionComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule, 
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
